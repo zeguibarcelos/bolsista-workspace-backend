2c8f60eeb3edc3d86396f1ea9efb21e8f8fb720d
zeguibarcelos <116059956+zeguibarcelos@users.noreply.github.com>
2023-04-27 03:56:02 -0300

    db_host

--- src/database/index.ts
+++ src/database/index.ts
@@ -7,7 +7,7 @@ import { Localidade } from "../entities/Localidade"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     port: 3306,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
